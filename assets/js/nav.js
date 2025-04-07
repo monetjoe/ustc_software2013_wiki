ec04f872beaf0f4be8ca3114c3a3e26bedd7af9a
admin <admin@noreply.com>
2025-04-07 14:30:00 +0800

    fixing method

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $('head').prepend('<link rel="shortcut icon" href="https://2013.igem.org/favicon.ico">');
     $('body').prepend('<div id="nav"></div>');
     $('#nav').load('nav.html');
+    $('a').each(function () {
+        if (!$(this).attr('href').includes("#")) $(this).attr('target', '_blank');
+    });
 });
